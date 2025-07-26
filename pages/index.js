8256f1b083e08b89a12f87804fcb8fbf73f25ae2
carlos ortega <sr.carlosortega@gmail.com>
2025-07-26 15:05:31 -0300

    feat: integrate Firebase Firestore for task management
    - Added Firebase as a dependency and configured Firestore in a new firebaseConfig.js file.
    - Updated the task management logic in index.js to use Firestore for adding, updating, and managing tasks.
    - Modified the agregarTarea, estadoTarea, and editarTarea functions to interact with Firestore.

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,73 @@
 import TareasForm from "@/components/TareasForm";
 import { useState } from "react";
 import { ListaTareas } from "@/components/ListaTareas";
+import { db } from "@/services/firebaseConfig";
+import {
+  addDoc,
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
 export default function Home() {
   const [tareas, setTareas] = useState([]);
   const [idTareaEditar, setIdTareaEditar] = useState(null);
 
-  const agregarTarea = (tarea) => {
-    const nuevaTarea = {
-      id: Date.now(),
-      tarea,
-      completada: false,
-    };
-    setTareas([...tareas, nuevaTarea]);
-    console.log("Tarea agregada:", nuevaTarea);
+  const agregarTarea = async (tarea) => {
+    // console.log("Tarea agregada:", nuevaTarea);
+    try {
+      const docRef = await addDoc(collection(db, "tareas"), {
+        tarea: tarea,
+        completada: false,
+      });
+
+      const nuevaTarea = {
+        id: docRef.id,
+        tarea,
+        completada: false,
+      };
+      setTareas([...tareas, nuevaTarea]);
+      console.log("Tarea guardada en Firestore con ID:", docRef.id);
+    } catch (error) {
+      console.error("Error al agregar la tarea:", error);
+    }
   };
 
-  const estadoTarea = (id) => {
+  const estadoTarea = async (id) => {
     const tareasActualizadas = tareas.map((tarea) =>
       tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea
     );
+
     setTareas(tareasActualizadas);
+
+    const tareaActualizada = tareasActualizadas.find(
+      (tarea) => tarea.id === id
+    );
+    const ref = doc(db, "tareas", id);
+    await updateDoc(ref, {
+      completada: tareaActualizada.completada,
+    });
   };
 
   const eliminarTarea = (id) => {
     const tareasActualizadas = tareas.filter((tarea) => tarea.id !== id);
     setTareas(tareasActualizadas);
   };
-  const editarTarea =( id , nuevaTarea) =>{
+
+  const editarTarea = async (id, nuevaTarea) => {
     const tareaNueva = tareas.map((tarea) =>
-      tarea.id === id ? {...tarea, tarea: nuevaTarea} : tarea
+      tarea.id === id ? { ...tarea, tarea: nuevaTarea } : tarea
     );
     setTareas(tareaNueva);
+    const ref = doc(db, "tareas", id);
+    await updateDoc(ref, {
+      tarea: nuevaTarea,
+    });
     setIdTareaEditar(null);
+  };
 
-  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">Gestor de Tareas</h1>
---
+++ services/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9E0sQ7smgq36G7Sk251c9_azLZPrg4E",
+  authDomain: "app-tareas-764e3.firebaseapp.com",
+  projectId: "app-tareas-764e3",
+  storageBucket: "app-tareas-764e3.firebasestorage.app",
+  messagingSenderId: "485994068374",
+  appId: "1:485994068374:web:34fc94b52f44db6d7685b7",
+  measurementId: "G-PL0X0CVD2C",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
